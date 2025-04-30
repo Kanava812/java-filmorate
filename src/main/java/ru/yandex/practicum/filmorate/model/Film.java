dce6a35d9f713dc89c7b68575b65039528b92093
Kanava <knvznr@yandex.ru>
2025-04-30 03:37:42 +0300

    фз№10

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,30 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Film {
+     private long id;
+
+     @NotNull(message = "Не может быть null.")
+     @NotBlank(message = "Не может быть пустым.")
+     private String name;
+
+     @Size(min = 1, max = 200, message = "Не более 200 символов.")
+     private String description;
+
+     @NotNull(message = "Не может быть null.")
+     private LocalDate releaseDate;
+
+     @Positive(message = "Длительность фильма должна быть положительным числом.")
+     private int duration;
 }
+
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/users")
+@Data
+public class User {
+    private long id;
+
+    @Email(message = "Адрес должен содержать символ @.")
+    @NotNull(message = "Не может быть null.")
+    @NotBlank
+    private String email;
+
+    @NotBlank(message = "Логин не может быть пустым.")
+    @Pattern(regexp = "^\\S+$", message = "Логин не должен содержать пробелы.")
+    @NotNull(message = "Не может быть null.")
+    private String login;
+
+    private String name;
+
+    @PastOrPresent(message = "ДР не может быть в будущем.")
+    private LocalDate birthday;
+}
